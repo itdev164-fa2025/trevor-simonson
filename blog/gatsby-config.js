601b86a9d3ae2a787711772efe82da39823e4b97
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-08-28 17:11:04 -0500

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -9,10 +9,15 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `The Shiznit`,
+    author: `Trevor Simonson`,
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    contact:{
+      name: `Trevor Simonson`,
+      company: `Blogs Inc.`,
+      address: `PO Box 1324`
+    }
   },
   plugins: [
     `gatsby-plugin-image`,
