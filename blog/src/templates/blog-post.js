d5c883f3a12ef50980c223430a4e494c07f903d2
Trevor Simonson <simonsot@gmatc.matc.edu>
2024-09-13 14:25:15 -0500

    Updated pages to show additional content

--- blog/src/templates/blog-post.js
+++ blog/src/templates/blog-post.js
@@ -4,22 +4,28 @@ import Layout from '../components/layout'
 
 const BlogPost = ({ data }) => {
     console.log(data);
-    const {title} = data.contentfulBlogPost;
+    const {title, body} = data.contentfulBlogPost;
 
     return (
         <Layout>
-        <h1>{title}</h1>
+            <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
-    )
+    );
 }
 
 export default BlogPost;
 
 export const pageQuery = graphql`
-    query blogPostQuery($slug: String) {
+    query blogPostQuery($slug: String!) {
         contentfulBlogPost(slug: {eq: $slug}) {
             title
             slug
+        	body{
+              childMarkdownRemark{
+                html
+              }
+            }
         }
     }
 `
